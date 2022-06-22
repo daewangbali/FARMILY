5c4a8309a9bdf1049074551c98b773391c76da92
daewangbali <daewangbali@gmail.com>
2022-06-22 10:41:35 +0900

    20220622/dawoon/left.html수정

--- finalproject_farmily/src/main/java/org/kosta/myproject/controller/BoardController.java
+++ finalproject_farmily/src/main/java/org/kosta/myproject/controller/BoardController.java
@@ -1,14 +1,26 @@
 package org.kosta.myproject.controller;
 
+import java.util.List;
+
 import org.kosta.myproject.service.BoardService;
+import org.kosta.myproject.vo.BoardVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
-	private final BoardService boardService;
+private final BoardService boardService;
+	
+	@RequestMapping("guest/boardListByBoardCategori")
+	public String findBoardListByBoardCategori(String boardCategori,Model model) {
+		List<BoardVO> list = boardService.findBoardListByBoardCategori(boardCategori);
+		model.addAttribute("boardList",list);
+		return "board/board-categori-list";
+	}
 	
 	
 }
